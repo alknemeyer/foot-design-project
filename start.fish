a58455c19247b83d3b5241c7d76453df609db3be
Alexander Knemeyer <alknemeyer@gmail.com>
2020-10-22 11:37:50 +0200

    Document

--- start.fish
+++ start.fish
@@ -1,12 +1,17 @@
 #! /usr/bin/fish
 # chmod +x start.fish
 
+# fix permission errors from serial <-> OptoForce
 sudo chmod 666 /dev/ttyACM0
 
+# script on my computer that activates conda. I do this
+# manually to stop the constant (base) thing
 source-conda;
 
 and conda activate foot-design;
 
+# if the script is run as `./startfish i`,
+# launch ipython
 and if test "$argv[1]" = "i"
     ipython
 end
