da8b08736a68f71cd6ad90fe4a28de9668bc62f7
Alexander Knemeyer <alknemeyer@gmail.com>
2020-10-22 11:05:30 +0200

    Add styling; animation code

--- scripts/plot_optoforce.py
+++ scripts/plot_optoforce.py
@@ -8,6 +8,7 @@ Run as follows:
 """
 import matplotlib.pyplot as plt
 import numpy as np
+plt.style.use('seaborn')
 
 
 def convert(s: bytes):
@@ -23,10 +24,41 @@ arr: np.ndarray = np.loadtxt(
 )
 
 tstart = arr[0, 0]
-plt.plot(arr[:, 0] - tstart, arr[:, 1:])
+t = arr[:, 0] - tstart
+plt.plot(t, arr[:, 1:])
 plt.grid(True)
 plt.title(f'OptoForce - logged data - {arr.shape[0]} data points')
 plt.legend(('Fx', 'Fy', 'Fz'))
 plt.xlabel('Time [$s$]')
 plt.ylabel('Force [$N$]')
 plt.show()
+
+###
+
+# from matplotlib.animation import FuncAnimation
+# fig, ax = plt.subplots()
+# linex, = ax.plot([], [])
+# liney, = ax.plot([], [])
+# linez, = ax.plot([], [])
+# def init():
+#     plt.grid(True)
+#     plt.title(f'Force data')
+#     plt.legend(('Fx', 'Fy', 'Fz'))
+#     plt.xlabel('Time [$s$]')
+#     plt.ylabel('Force [$N$]')
+#     ax.set_xlim(np.min(t), np.max(t))
+#     ax.set_ylim(np.min(arr[:,1:]), np.max(arr[:,1:]))
+#     return linex, liney, linez
+
+# def update(frame: int):
+#     linex.set_data(t[:frame], arr[:frame, 1])
+#     liney.set_data(t[:frame], arr[:frame, 2])
+#     linez.set_data(t[:frame], arr[:frame, 3])
+#     return linex, liney, linez
+
+# stride = 50
+# nsamples = len(t)
+# dt_ms = 1000*t[-1]/nsamples * stride
+# ani = FuncAnimation(fig, update, init_func=init, interval=dt_ms, frames=range(0, nsamples, stride), blit=True)
+# # plt.show()
+# ani.save('data/force-animation.mp4')
